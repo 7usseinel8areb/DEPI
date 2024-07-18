c7f80aa319107d756ca879a99f443f99e4502257
Hussein Elghareb <halghryb287@gmail.com>
2024-07-18 20:00:01 +0300

    Assignment pdf Left join answers

--- MS SQL Server/Session3/Assignment Ans.sql
+++ MS SQL Server/Session3/Assignment Ans.sql
@@ -54,3 +54,34 @@ join sales.orders ord on sto.store_id = ord.store_id
 join sales.order_items orditem on ord.order_id = orditem.order_id
 join production.products pro on pro.product_id = orditem.product_id
 group by sto.store_id,store_name
+
+
+
+
+-- 1. List all products along with their categories. Include products that do not belong to any category.
+select * from production.products pro 
+left join production.categories cat on pro.category_id = cat.category_id
+
+-- 2. Find the total quantity of each product sold. Include products that have never been sold.
+select pro.product_id,sum(ord.quantity) 'Quantity Sold'from production.products pro 
+left join sales.order_items ord on ord.product_id =pro.product_id
+group by pro.product_id
+
+-- 3. Retrieve the details of all orders along with the customer name. Include orders placed by customers who are not yet registered.
+select ord.*,cus.first_name from sales.orders ord 
+left join sales.customers cus on ord.customer_id = cus.customer_id
+
+-- 4. List all staff members along with the store they are assigned to. Include staff members who are not currently assigned to any store
+select * from  sales.staffs sta 
+left join sales.stores sto on sta.store_id = sto.store_id
+
+--5. Find the average rating of products in each category. Include categories that do not have any products yet.
+select * /*cat.category_id,cat.category_name,Avg()*/ from production.categories cat 
+left join production.products pro on cat.category_id  = pro.category_id
+--Group by cat.category_id,cat.category_name
+
+
+-- 6. Get the list of all customers along with the total number of orders they have placed.Include customers who have not placed any orders.
+select cus.customer_id, cus.first_name, COUNT(ord.order_id) 'Total orders' from sales.customers cus 
+join sales.orders ord on cus.customer_id = ord.customer_id
+group by cus.customer_id, cus.first_name
